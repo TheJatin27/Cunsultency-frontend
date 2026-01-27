c823a446869131cedc3d9847e961e5d59ddfd86f
Jatin Mishra <70195183+TheJatin27@users.noreply.github.com>
2026-01-27 18:29:55 +0530

    Fix base path for build command in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-     base: "/Cunsultency-frontend/",
+    base: command === "build" ? "/Cunsultency-frontend/" : "/",
   };
 });
